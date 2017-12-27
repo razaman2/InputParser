af4bf705d330b999e03f73ac7b5b81655f3c1031
razam <razamvmt@gmail.com>
2017-12-27 17:21:21 -0500

    parsers update

--- Parser/Parsers/ObjectParser.php
+++ Parser/Parsers/ObjectParser.php
@@ -2,29 +2,7 @@
 
 	namespace Parser\Parsers;
 
-	class ObjectParser implements \Parser\Interfaces\Parsable
+	class ObjectParser extends AbstractParsers
 	{
-		protected $inputData;
 
-		public function __construct(array $inputData) {
-
-			$this->inputData = $inputData;
-		}
-
-		public function data() {
-
-			return $this->assignDataToObject();
-		}
-
-		protected function assignDataToObject() {
-
-			$dataObject = new \Parser\ParserDataObject($this->inputData);
-
-			foreach($this->inputData as $key => $val) {
-
-				$dataObject->$key = $val;
-			}
-
-			return $dataObject;
-		}
 	}
--- Parser/Runners/parse.php
+++ Parser/Runners/parse.php
@@ -3,22 +3,20 @@
 	require_once '../../vendor/autoload.php';
 
 	$obj = new stdClass();
-	$obj->first_name = 'Miyosha';
-	$obj->last_name = 'Sparkes';
+	$obj->first_name = 'first';
+	$obj->last_name = 'last';
 	$obj->age = 29;
 	$obj->married = true;
 	$obj->test = null;
-	$obj->pregnant = false;
+	$obj->real = false;
 	$obj->height = 5.6;
 
 	try {
 
-		$inputData = (new \Parser\Parser())->parse('');
+		$inputData = (new \Parser\Parser())->parse('{"name":"jim brown","location":{"address1":"25 main street","address2":"suite 105","city":"paterson","state":"nj","zip":"07221","county":"passaic","country":"usa"}}');
 	} catch(\Parser\Exceptions\InvalidInputDataException $e) {
 
 		echo $e->getMessage();
 	}
 
 	print_r($inputData);
-//	echo "hello, my name is {$inputData->first_name} {$inputData->last_name}. i am {$inputData->age} years old
-//and currently married {$inputData->married}. my height is $inputData->height";
